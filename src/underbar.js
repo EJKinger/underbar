83ec8f088663d31514a585b5678e0c64fa42341b
Eric King <e.king117@gmail.com>
2015-10-29 23:04:45 -0400

    added idea for sortby

--- src/underbar.js
+++ src/underbar.js
@@ -399,6 +399,7 @@
   // of that string. For example, _.sortBy(people, 'name') should sort
   // an array of people by their name.
   _.sortBy = function(collection, iterator) {
+    collection.sort(iterator);
   };
 
   // Zip together two or more arrays with elements of the same index
@@ -482,5 +483,10 @@
   //
   // Note: This is difficult! It may take a while to implement.
   _.throttle = function(func, wait) {
+    return function(func, wait){setTimeout(function(func){
+      console.log('called');
+      func();
+    }, wait);
+  };
   };
 }());
